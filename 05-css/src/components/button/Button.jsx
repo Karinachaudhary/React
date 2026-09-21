3fd30a54892ed4c048566ba79b864d1093398881
Karina Chaudhary <karinachaudharyyy@gmail.com>
2026-09-22 02:40:10 +0545

    css

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './button.module.css'
+
+const Button = () => {
+  return (
+    <div className={Style.btn}>
+      Dabaa rey
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './header.module.css'
+const Header = () => {
+  return (
+    <div className={Style.header}>
+      Main hun topper
+      <button className='btn'>Areyyy pehele idhar</button>
+    </div>
+  )
+}
+
+export default Header
